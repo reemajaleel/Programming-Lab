d0443d70903c89281f53ceb0ba7eaff01361fd88
Reema Jaleel <93534699+reemajaleel@users.noreply.github.com>
2022-01-19 15:53:40 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,7 @@
+import math
+def cirarea(r):
+    result=math.pi*r*r
+    return result
+def cirperi(r):
+    result=2*math.pi*r
+    return result
---
+++ rectangle.py
@@ -0,0 +1,6 @@
+def rectarea(l,b):
+    area=l*b
+    return area
+def rectperi(l,b):
+    peri=2*(l+b)
+    return peri
